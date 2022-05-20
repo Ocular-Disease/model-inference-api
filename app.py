607f4605e37f7847135227cfbfc6604fa2d54727
darklight147 <39389636+darklight147@users.noreply.github.com>
2022-05-20 22:59:17 +0100

    feat: add middleware

--- app.py
+++ app.py
@@ -6,10 +6,26 @@ import io
 import boto3
 import os
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # from enviroment variables
 s3 = boto3.Session(
